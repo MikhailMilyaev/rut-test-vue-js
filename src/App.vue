0714196fab6dc5c14819cb85cdf42142eb25898a
MikhailMilyaev <milyaevmisha@gmail.com>
2026-03-26 17:27:44 +0300

    feat: add Sidebard component placeholder and grid layout for all app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <script setup>
 import Header from './components/Header.vue';
+import SideBar from './components/SideBar.vue';
 
 </script>
 
 <template>
-  <Header></Header>
+  <div class="app-wrapper">
+    <Header></Header>
+    <div class="main-layout">
+      <SideBar></SideBar>
+      <main class="content-area">
+        <h1>Area</h1>
+      </main>
+    </div>
+  </div>
+  
 </template>
+
+<style scoped>
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.main-layout {
+  display: grid;
+  grid-template-columns: 310px 1fr;
+  flex: 1;
+}
+
+.content-area {
+  overflow-y: auto;
+  padding: 0 40px;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <aside class="sidebar"></aside>
+</template>
+
+<style scoped>
+.sidebar {
+    width: 310px;
+    height: 100%;
+}
+</style>
